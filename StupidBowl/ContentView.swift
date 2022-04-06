19d52c9b3c07bcd09ebe30e19995cc59e673fdf1
Timothy Price <Imnotsaying@gmail.com>
2022-04-06 10:22:03 -0500

    Initial Commit

---
+++ StupidBowl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StupidBowl
+//
+//  Created by Student on 4/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StupidBowl/StupidBowlApp.swift
@@ -0,0 +1,17 @@
+//
+//  StupidBowlApp.swift
+//  StupidBowl
+//
+//  Created by Student on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct StupidBowlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
