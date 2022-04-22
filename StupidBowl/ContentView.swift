9510009a6b0e1815c60c66dc79a3ae22aa78fac9
Colin Farley <colinfarley@yahoo.com>
2022-04-22 10:47:36 -0500

    scuffed background

--- StupidBowl/ContentView.swift
+++ StupidBowl/ContentView.swift
@@ -4,13 +4,22 @@
 //
 //  Created by Student on 4/6/22.
 //
-
+import UIKit
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+            Text("Welcome To Stupid Bowl!")
+        NavigationView{
+        NavigationLink( destination: GameScreen(), label: {
+            ZStack {
+            Image("banner")
+                Text("Continue")
+                .bold()
+                
+            }
+        })
+        }
     }
 }
 
---
+++ StupidBowl/GameScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  StupidBowl
+//
+//  Created by Student on 4/22/22.
+//
+
+import SwiftUI
+
+struct GameScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameScreen()
+    }
+}
